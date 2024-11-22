dfa14f8d8da0cfb3e9ab34b1723bcf6b80375e76
sholehbaktiabadi <sholehbaktiabadi@gmail.com>
2024-11-22 20:11:15 +0700

    feat: form signup automation

--- app.ts
+++ app.ts
@@ -1,5 +1,8 @@
 import MailSlurp from "mailslurp-client";
-import * as puppeteer from "puppeteer"
+import puppeteer from "puppeteer-extra"
+import stealthPlugin from "puppeteer-extra-plugin-stealth"
+
+puppeteer.use(stealthPlugin())
 
 const delay = (delayInms: number) => {
     return new Promise(resolve => setTimeout(resolve, delayInms));
@@ -12,22 +15,21 @@ async function bootstrap(){
     const page = await browser.newPage()
     page.setViewport({ width: 1920, height: 1200 })
     await page.goto("https://www.upwork.com/nx/signup/?dest=home")
-    // await page.waitForSelector("input")
     await page.click("#main > div > div > div > div > div > div > div:nth-child(2) > fieldset > div > div:nth-child(2) > div > input")
     await page.click("#main > div > div > div > div > div > div > div.mt-6x.mt-md-10x > button")
     await delay(2000)
-    // await page.waitForSelector("input")
     await page.type("#first-name-input", "sholeh", { delay: 100 })
     await page.type("#last-name-input", "bakti abadi", { delay: 100 })
 
     await page.type("#redesigned-input-email", "sholehbaktiabadi@gmail.com", { delay: 100 })
     await page.type("#password-input", "NamaHewan1!", { delay: 100 })
-    // await page.click("#main > div > div > div > div > div > div > div.mt-6x.mt-md-10x > button")
-    await page.select("#dropdown-menu > li.is-active.is-focused.air3-menu-item > span", "Indonesia")
+    // await page.click("#country-dropdown > div > div > span")
+    await page.click("#checkbox-terms > span.air3-checkbox-fake-input")
+    // await page.click("#button-submit-form")
     await delay(2000)
     const lastEmail = await mailslurp.waitForLatestEmail('a0a692f7-3877-43f1-bbef-331dc79c873a')
     console.log(lastEmail, "latestEmail")
-    browser.close()
+    // browser.close()
 }
 
 bootstrap()
